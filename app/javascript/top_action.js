e6f7264b7c4df3696a19aa205440c69a8bcdaf3d
LIFE-enjoy <hndke10ve@icloud.com>
2021-04-23 19:14:22 +0900

    全体のビューの変更

--- app/javascript/top_action.js
+++ app/javascript/top_action.js
@@ -1,6 +1,8 @@
-function topAction(){
-  const topLog = document.querySelectorAll('.start-log')
-  if (!topLog){ return false;}
-}
-
-window.addEventListener('load',topAction)
+$(function() {
+	setTimeout(function(){
+		$('.start-log').fadeIn(800);
+	},500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start').fadeOut(700);
+	},2000); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
